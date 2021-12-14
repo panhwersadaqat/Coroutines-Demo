65c74156b1b98b0088e671c15f2a6eb9a83f4a0f
Sadaqat Panhwer <panhwersadaqat22@gmail.com>
2021-12-14 16:29:36 +0500

    first commit

---
+++ app/src/main/java/com/example/coroutinesdemo/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.coroutinesdemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+class MainActivity : AppCompatActivity() {
+
+    private var count = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnCount.setOnClickListener {
+            tvCount.text = count++.toString()
+        }
+        btnDownloadUserData.setOnClickListener {
+            CoroutineScope(Dispatchers.Main).launch {
+                //downloadUserData()
+                tvUserMessage.text = UserDataManager2().getTotalUserCount().toString()
+            }
+        }
+
+    }
+
+    private suspend  fun downloadUserData() {
+            for (i in 1..200000) {
+                withContext(Dispatchers.Main) {
+                    tvUserMessage.text = "Downloading user $i in ${Thread.currentThread().name}"
+                }
+            }
+    }
+}
